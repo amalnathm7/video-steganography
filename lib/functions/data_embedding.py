06002495dac60e607d93ce227162f7067562e9e0
Amal Nath M <amalnathm7@gmail.com>
2023-01-03 14:05:49 +0530

    PSNR and SSIM

--- lib/functions/data_embedding.py
+++ lib/functions/data_embedding.py
@@ -2,12 +2,29 @@ import cv2
 import math
 
 
+def option(opt):
+    match opt:
+        case 1: return "akiyo"
+        case 2: return "bowing"
+        case 3: return "bus"
+        case 4: return "city"
+        case 5: return "crew"
+
+
 def decimalToBinary(n):
     return bin(n).replace("0b", "").zfill(8)
 
 
+flag = False
+while not flag:
+    print("1. akiyo\n2. bowing\n3. bus\n4. city\n5. crew\n")
+    opt = int(input("Select cover video: "))
+    if (opt > 0 and opt < 6):
+        flag = True
+
 # read video from file
-cap = cv2.VideoCapture('assets/cover_videos/akiyo_cif.y4m')
+filename = option(opt)
+cap = cv2.VideoCapture(f'assets/cover_videos/{filename}_cif.y4m')
 video_fps = cap.get(cv2.CAP_PROP_FPS)
 total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -21,7 +38,7 @@ data = file.read()
 data = data + "$"
 
 fourcc = cv2.VideoWriter_fourcc(*'HFYU')
-writer = cv2.VideoWriter('assets/stego_videos/akiyo_stego.avi', apiPreference=0, fourcc=fourcc,
+writer = cv2.VideoWriter(f'assets/stego_videos/{filename}_stego.avi', apiPreference=0, fourcc=fourcc,
                          fps=video_fps, frameSize=(int(width), int(height)))
 
 flag = True
@@ -31,7 +48,7 @@ while True:
     if not ret:
         break  # break if no next frame
 
-    if(flag):
+    if (flag):
         for i in range(0, len(data)):
             binary = decimalToBinary(ord(data[i]))
 
--- lib/functions/data_extraction.py
+++ lib/functions/data_extraction.py
@@ -2,6 +2,15 @@ import cv2
 import math
 
 
+def option(opt):
+    match opt:
+        case 1: return "akiyo"
+        case 2: return "bowing"
+        case 3: return "bus"
+        case 4: return "city"
+        case 5: return "crew"
+
+
 def decimalToBinary(n, fill):
     return bin(n).replace("0b", "").zfill(fill)
 
@@ -10,8 +19,16 @@ def binaryToDecimal(n):
     return int(n, 2)
 
 
+flag = False
+while not flag:
+    print("1. akiyo\n2. bowing\n3. bus\n4. city\n5. crew\n")
+    opt = int(input("Select stego video: "))
+    if (opt > 0 and opt < 6):
+        flag = True
+
 # read video from file
-cap = cv2.VideoCapture('assets/stego_videos/akiyo_stego.avi')
+filename = option(opt)
+cap = cv2.VideoCapture(f'assets/stego_videos/{filename}_stego.avi')
 video_fps = cap.get(cv2.CAP_PROP_FPS)
 total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -36,7 +53,8 @@ while True:
 
             if (ch == '$'):
                 file = open("assets/extracted_files/texts/output.txt", "w")
-                print("Output file at assets/extracted_files/texts/output.txt successfully created")
+                print(
+                    "Output file at assets/extracted_files/texts/output.txt successfully created")
                 file.write(data)
                 flag = True
                 break

bf4cc5ac4cb756df7ff811460373d5be081f1081
Amal Nath M <amalnathm7@gmail.com>
2023-02-13 13:59:20 +0530

    Input text

--- lib/modules/data_embedding.py
+++ lib/modules/data_embedding.py
@@ -16,80 +16,106 @@ def decimalToBinary(n):
     return bin(n).replace("0b", "").zfill(8)
 
 
-file = open("assets/secret_files/texts/input.txt", "r")
-data = file.read()
-data = data + "$"
+def text_embedding():
+    file = open("assets/secret_files/texts/input.txt", "r")
+    data = file.read()
+    data = data + "$"
 
-flag = False
-while not flag:
-    print("1. akiyo\n2. bowing\n3. bus\n4. city\n5. crew\n")
-    opt = int(input("Select cover video: "))
-    if (opt > 0 and opt < 6):
-        # read video from file
-        filename = option(opt)
-        cap = cv2.VideoCapture(f'assets/cover_videos/{filename}_cif.y4m')
-        video_fps = cap.get(cv2.CAP_PROP_FPS)
-        total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    flag = True
+    while flag:
+        print("\n1. akiyo\n2. bowing\n3. bus\n4. city\n5. crew\n")
+        opt = int(input("Select cover video: "))
 
-        if (len(data) > height * width):
-            print("Input file is too big. Use a different video file.\n")
+        if (opt > 0 and opt < 6):
+            filename = option(opt)
+            cap = cv2.VideoCapture(f'assets/cover_videos/{filename}_cif.y4m')
+
+            video_fps = cap.get(cv2.CAP_PROP_FPS)
+            total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+            height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            flag = False
         else:
-            flag = True
+            print("Invalid option!")
 
-print(
-    f"Frame Per second: {video_fps}\nTotal Frames: {total_frames}\nHeight: {height}\nWidth: {width}")
+    print(
+        f"\nVideo: {filename}\nFrame per second: {video_fps}\nTotal frames: {total_frames}\nHeight: {height}\nWidth: {width}")
 
-if not os.path.exists('assets/stego_videos'):
-   os.makedirs('assets/stego_videos')
-fourcc = cv2.VideoWriter_fourcc(*'HFYU')
-writer = cv2.VideoWriter(f'assets/stego_videos/{filename}_stego.avi', apiPreference=0, fourcc=fourcc,
-                         fps=video_fps, frameSize=(int(width), int(height)))
+    if not os.path.exists('assets/stego_videos'):
+        os.makedirs('assets/stego_videos')
+    fourcc = cv2.VideoWriter_fourcc(*'HFYU')
+    writer = cv2.VideoWriter(f'assets/stego_videos/{filename}_stego.avi', apiPreference=0, fourcc=fourcc,
+                             fps=video_fps, frameSize=(int(width), int(height)))
 
-flag = True
+    flag = True
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break  # break if no next frame
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    if (flag):
-        flag = False
+        if (flag):
+            flag = False
 
-        count = 0
+            count = 0
 
-        for i in range(0, int(height)):
-            flag1 = False
+            for i in range(0, int(height)):
+                flag1 = False
 
-            for j in range(0, int(width)):
-                binary = decimalToBinary(ord(data[count]))
+                for j in range(0, int(width)):
+                    binary = decimalToBinary(ord(data[count]))
 
-                frame[i, j, 0] = math.floor(
-                    frame[i, j, 0] / 8) * 8 + (int(binary[2]) + int(binary[1]) * 2 + int(binary[0]) * 4)
-                frame[i, j, 1] = math.floor(
-                    frame[i, j, 1] / 8) * 8 + (int(binary[5]) + int(binary[4]) * 2 + int(binary[3]) * 4)
-                frame[i, j, 2] = math.floor(
-                    frame[i, j, 2] / 4) * 4 + (int(binary[7]) + int(binary[6]) * 2)
+                    frame[i, j, 0] = math.floor(
+                        frame[i, j, 0] / 8) * 8 + (int(binary[2]) + int(binary[1]) * 2 + int(binary[0]) * 4)
+                    frame[i, j, 1] = math.floor(
+                        frame[i, j, 1] / 8) * 8 + (int(binary[5]) + int(binary[4]) * 2 + int(binary[3]) * 4)
+                    frame[i, j, 2] = math.floor(
+                        frame[i, j, 2] / 4) * 4 + (int(binary[7]) + int(binary[6]) * 2)
 
-                count = count+1
+                    count = count+1
 
-                if (count == len(data)):
-                    flag1 = True
+                    if (count == len(data)):
+                        flag1 = True
+                        break
+
+                if (flag1):
                     break
 
-            if (flag1):
-                break
+        writer.write(frame)
 
-    writer.write(frame)  # write frame
+    print("\nEmbedded successfully")
+    writer.release()
+    cap.release()
 
-    # cv2.imshow('Akiyo Frames', frame)  # show frame
 
-    # if cv2.waitKey(1) & 0xFF == ord('q'):  # on press of q break
-    #     break
+def image_embedding():
+    print("")
 
-# release and destroy windows
-print("Embedded successfully")
-writer.release()
-cap.release()
-cv2.destroyAllWindows()
+
+def video_embedding():
+    print("")
+
+
+def main():
+    print("\nVideo Steganography")
+    flag = True
+
+    while (flag):
+        print("\n1. Text\n2. Image\n3. Video\n")
+        file_type = int(input("Select secret file type: "))
+
+        flag = False
+
+        if (file_type == 1):
+            text_embedding()
+        elif (file_type == 2):
+            image_embedding()
+        elif (file_type == 3):
+            video_embedding()
+        else:
+            print("Invalid option!")
+            flag = True
+
+
+if __name__ == '__main__':
+    main()

6a4b4b4160db3edde2ad5184821c4e13675aa343
Meenakshi Nair <meenakshin@firstbytedigital.com>
2023-02-21 17:52:44 +0530

    Histogram method

--- lib/functions/frame_selection.py
+++ lib/functions/frame_selection.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import cv2
 import heapq
 from sewar import ssim
 
@@ -11,31 +13,98 @@ def option(opt):
         case 4: return "city"
         case 5: return "crew"
 
+# METHOD 3: Histogram difference
+
+
+def get_k_smallest(test_list, k):
+    return sorted(range(len(test_list)), key=lambda sub: test_list[sub])[:k]
+
+
+def get_k_largest(test_list, k):
+    return sorted(range(len(test_list)), key=lambda sub: test_list[sub])[-k:]
+
+
+cap = cv2.VideoCapture('assets/cover_videos/bus_cif.y4m')
+
+key_frames = []
+hist_list = []
+prev_hist = None
+frame_count = 0
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+    hist = cv2.normalize(hist, hist).flatten()
+
+    # hist = cv2.calcHist([frame], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
+    # hist = cv2.normalize(hist, hist).flatten()
+
+    if prev_hist is None:
+        prev_hist = hist
+        continue
+
+    hist_list.append(cv2.compareHist(prev_hist, hist, cv2.HISTCMP_CHISQR))
+    prev_hist = hist
+    frame_count += 1
+
+count = int(input("Enter number of frames: "))
+
+key_frames = get_k_smallest(hist_list, count)
+
+for frame_num in key_frames:
+    print(frame_num)
+
+cap.release()
+cap = cv2.VideoCapture('assets/cover_videos/bus_cif.y4m')
+count = 0
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    if count in key_frames:
+        cv2.imshow('Akiyo', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    count += 1
+
+cap.release()
+cv2.destroyAllWindows()
 
 # METHOD 2: Fibonacci with a key
-nterms = int(input("No. of frames required:  "))
-key = int(input("Enter the key: "))
-n1, n2 = 0, 1
-count = 0
-key_frames = []
-print("1. akiyo\n2. bowing\n3. bus\n4. city\n5. crew\n")
-opt = int(input("Select cover video: "))
-if (opt > 0 and opt < 6):
-   # read video from file
-    filename = option(opt)
-    cap = cv2.VideoCapture(f'assets/cover_videos/{filename}_cif.y4m')
-    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-if nterms <= 0:
-    print("Please enter a positive integer")
-else:
-    print("Fibonacci sequence:")
-    while count < nterms:
-        # print(n1)
-        key_frames.append(n1)
-        nth = ((n1 + n2) + key) % key
-        n1 = n2
-        n2 = nth
-        count += 1
+# nterms = int(input("No. of frames required:  "))
+# key = int(input("Enter the key: "))
+# n1, n2 = 0, 1
+# count = 0
+# key_frames = []
+# print("1. akiyo\n2. bowing\n3. bus\n4. city\n5. crew\n")
+# opt = int(input("Select cover video: "))
+# if (opt > 0 and opt < 6):
+#    # read video from file
+#     filename = option(opt)
+#     cap = cv2.VideoCapture(f'assets/cover_videos/{filename}_cif.y4m')
+#     total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+# if nterms <= 0:
+#     print("Please enter a positive integer")
+# else:
+#     print("Fibonacci sequence:")
+#     while count < nterms:
+#         # print(n1)
+#         key_frames.append(n1)
+#         nth = ((n1 + n2) + key) % total_frames
+#         n1 = n2
+#         n2 = nth
+#         count += 1
+
+
 # METHOD 1: SSIM Merthod
 # def get_frame_numbers_of_smallest_k(ssim_list, opt1):
 #     return [i for i, x in heapq.nsmallest(opt1, enumerate(ssim_list

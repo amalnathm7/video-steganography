ba08c4440f05f39f3ce5330c0ed663c3b02c76b3
Amal Nath M <amalnathm@Amals-MacBook-Pro.local>
2023-05-15 00:06:43 +0530

    Version 1.0.0 completed

--- lib/modules/decryption/text_decryption.py
+++
@@ -1,9 +0,0 @@
-from Cryptodome.Cipher import AES
-from Cryptodome.Util.Padding import unpad
-
-def decrypt_text(ciphertext, iv, key):
-    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-    
-    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
-    
-    return plaintext.decode()
